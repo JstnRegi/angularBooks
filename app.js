7874a5d330e26e014bd2cf7e60b78d65dbaf4cdd
Justin Reyes <justin.regi.reyes@gmail.com>
2015-10-15 11:35:24 -0700

    added module 'angularBooks' and and Resource Controller

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 var app = angular.module('angularBooks', ['ngResource']);
+
+angular.module('angularBooks').service('Book', function($resource) {
+	return $resource('http://daretodiscover.herokuapp.com/books/:id')
+})
+
+angular.controller('ResourceController', function($scope, Book) {
+	$scope.book = Book.get({id: 200}, function(data) {
+		console.log(data);
+	})
+})
